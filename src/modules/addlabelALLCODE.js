522b69314f6e20c64e275620bd9b695a3b838440
Arctictern265 <arctictern265@gmail.com>
2025-10-07 04:30:37 +0900

    fix undefined error

--- src/modules/addlabelALLCODE.js
+++ src/modules/addlabelALLCODE.js
@@ -16,24 +16,24 @@ export function addlabel(viewer) {
         let cartesian = viewer.scene.pickPosition(movement.position);
 
         viewer.entities.add({
-                position: cartesian,
-                point: { pixelSize: 10, color: Color.RED },
-                label: {
-                    text: `You clicked here`,
-                    font: "20px sans-serif",
-                    fillColor: Color.YELLOW,
-                    style: LabelStyle.FILL_AND_OUTLINE,
-                    outlineWidth: 2,
-                    verticalOrigin: VerticalOrigin.BOTTOM,
-                    disableDepthTestDistance: Number.POSITIVE_INFINITY,
-                    pixelOffset: new Cartesian2(0, -12),
-                },
-            });
+            position: cartesian,
+            point: { pixelSize: 10, color: Color.RED },
+            label: {
+                text: `You clicked here`,
+                font: "20px sans-serif",
+                fillColor: Color.YELLOW,
+                style: LabelStyle.FILL_AND_OUTLINE,
+                outlineWidth: 2,
+                verticalOrigin: VerticalOrigin.BOTTOM,
+                disableDepthTestDistance: Number.POSITIVE_INFINITY,
+                pixelOffset: new Cartesian2(0, -12),
+            },
+        });
 
         const picked = viewer.scene.pick(movement.position);
-
-        picked.color = Color.RED;
-
+        if (defined(picked) && defined(picked.color)) {
+            picked.color = Color.RED;
+        }
 
     }, ScreenSpaceEventType.LEFT_CLICK);
     return handler
